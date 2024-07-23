212b68dcf52ed1f49265cb3c382f6f51ca8283a2
abeRodz <abrahamrodz17@unitec.edu>
2024-07-22 23:21:41 -0600

    FIX: fixed bug with logo

--- logo.js
+++ logo.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
     logoLink.href = "https://lse.posgrados.fi.uba.ar/"; // Replace with your desired URL
     logoLink.target = "_blank"; // Optional:
     var logoImg = document.createElement("img");
-    logoImg.src = "/images/uba.png"; // Adjust path to your actual logo
+    logoImg.src = "./images/uba.png"; // Adjust path to your actual logo
     logoImg.alt = "Logo"; // Optional alt text for accessibility
     
     logoLink.appendChild(logoImg); 
